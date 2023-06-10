0d28771dacca80807ea0aef9235fe835af190d72
alphanse98 <alphanse@Apples-MacBook-Pro-4.local>
2023-06-10 18:34:37 +0530

    heder

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,35 @@ import background from '../assets/Group 304.png'
 
 function Header() {
   return (
+    <div className='headerContainer'>
    <header>
-   
+    
    <div className='HeaderBoxOne'>
-        <p className="headerPara fontFamily">Plan My Spaces.com</p>
-        <h1 className="HeaderHeading fontFamily">Perfect Plan for your</h1>
-        <h2 className="HeaderHeadingTwo fontFamily">Dream Home</h2>
-        <h3 className="HeaderHeadingThree ">Get your model plan Rs 1</h3>
-        <div className="HeadarBtns">
-        <button className="HeaderDownloadBtn">Download Free Plan</button>
-        <button className="HeaderEnquriryBtn">Make Enquriry</button>
+   <nav>
+        <ul className='navBar'>
+            <li><a href="default.asp" className='fontFamily'>Home</a></li>
+            <li><a href="news.asp" className='fontFamily'>Services</a></li>
+            <li><a href="contact.asp" className='fontFamily'>Galary</a></li>
+            <li><a href="about.asp" className='fontFamily'>About</a></li>
+        </ul>   
+    </nav>
+        <div className='HeaderTexBox'>
+            <p className="headerPara fontFamily">Plan My Spaces.com</p>
+            <h1 className="HeaderHeading fontFamily">Perfect Plan for your</h1>
+            <h2 className="HeaderHeadingTwo fontFamily">Dream Home</h2>
+            <h3 className="HeaderHeadingThree ">Get your model plan Rs 1</h3>
+            <div className="HeadarBtns">
+            <button className="HeaderDownloadBtn">Download Free Plan</button>
+            <button className="HeaderEnquriryBtn">Make Enquriry</button>
+            </div>
         </div>
     </div>
 
     <div className='HeaderBoxTwo'>
-        <img src={background}></img>
+        <img src={background} className='HeaderImg'></img>
     </div>
 
-   </header>
+   </header></div>
   );
 }
 
