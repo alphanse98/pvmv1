b4ff03dd44e0eabc3b220ecb2c7158c388826697
alphanse98 <alphanse@Apples-MacBook-Pro-4.local>
2023-06-11 11:40:25 +0530

    service

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,8 +8,24 @@ function Services() {
             <hi className="serviceHeading fontFamily">Services</hi> 
             <div className="serviceBoxs">
                 <div className="serviceBox">
-                    <img src={plan}></img>
-                    <p>2d PLan</p>
+                    <img src={plan} className='serviceBoxIcon'></img>
+                    <p className='sevixeBoxP '>2D Plan</p>
+                </div>
+                <div className="serviceBox">
+                    <img src={plan} className='serviceBoxIcon'></img>
+                    <p className='sevixeBoxP '>Elevation</p>
+                </div>
+                <div className="serviceBox">
+                    <img src={plan} className='serviceBoxIcon'></img>
+                    <p className='sevixeBoxP '>3D floor PLan</p>
+                </div>
+                <div className="serviceBox">
+                    <img src={plan} className='serviceBoxIcon'></img>
+                    <p className='sevixeBoxP '>2D Plan</p>
+                </div>
+                <div className="serviceBox">
+                    <img src={plan} className='serviceBoxIcon'></img>
+                    <p className='sevixeBoxP '>2D Plan</p>
                 </div>
             </div>
 
