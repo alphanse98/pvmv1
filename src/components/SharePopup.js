a7cd737d88665f9372b4dc1c89b608b8c8feaae1
alphanse98 <alphanse98@gmail.com>
2023-07-25 23:50:37 -0500

    shre popup fuction

--- src/components/SharePopup.js
+++ src/components/SharePopup.js
@@ -4,14 +4,24 @@ import closeIcon from "../assets/Close.svg";
 import fbShareIcon from "../assets/fbShare.svg"
 import whShareIcon from "../assets/whShare.svg"
 import instaShareIcon from "../assets/instaShare.svg"
-
+import { FacebookShareButton, WhatsappShareButton } from 'react-share';
 
 
 const SharePopup = () => {
+    const {formActive, } = useSelector((state) => state.shareFormSlice);
+    let tedturl = "amazom.com"
 
-    const {formActive, url} = useSelector((state) => state.shareFormSlice);
+    let caption="caption"
+    const InstagramShare = () => {
+      // Replace 'your-text-here' with the text you want to pre-fill in the Instagram caption (optional).
+      const shareUrl = `https://www.instagram.com/?url=${encodeURIComponent(tedturl)}&caption=${encodeURIComponent(caption)}`;
+    window.open(shareUrl, '_blank', 'height=600,width=800,resizable=1');
+    };
 
-
+    const copyUrl = ()=>{
+      // Copy the text inside the text field
+       navigator.clipboard.writeText(tedturl);
+    }
 
   return (
     <div>
@@ -24,14 +34,18 @@ const SharePopup = () => {
             <p className=' shareHeading serviceHeading fontFamily'>Share Link</p>
             {/* share icons  */}
             <div className='shrePopUpIcons'>
-                <img src={fbShareIcon}  alt='fbShareIcon'></img>
-                <img src={whShareIcon} alt='whShareIcon'></img>
-                <img src={instaShareIcon} alt='instaShareIcon'></img>
+                <FacebookShareButton url={tedturl}>
+                  <img src={fbShareIcon}  alt='fbShareIcon'></img>
+                </FacebookShareButton>  
+                <WhatsappShareButton url={tedturl}>
+                  <img src={whShareIcon} alt='whShareIcon'></img>
+                </WhatsappShareButton>
+                  <img src={instaShareIcon} onClick={()=>InstagramShare()} alt='instaShareIcon'></img>
             </div>
             <p className='copyLing fontFamily'>or copy link</p>
             <div className='shareLingBox'>
-                <p className='lingText fontFamily'>planMYspaces/plan/123257478215265256451456j</p>
-                <button className='HeaderDownloadBtn'>Copy</button>
+                <p className='lingText fontFamily'>{tedturl}</p>
+                <button className='HeaderDownloadBtn' onClick={()=>copyUrl()}>Copy</button>
             </div>
             </div>
         </div>
