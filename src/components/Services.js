85f7318dfa826b2483f2839a1208159bfd889f19
alphanse98 <alphanse98@gmail.com>
2023-08-13 17:37:21 +0530

    price

--- src/components/Services.js
+++ src/components/Services.js
@@ -13,28 +13,37 @@ function Services() {
                 <a className="serviceBox" href="#plan">
                     <img src= {require("../assets/elevation1.jpeg")} className='serviceBoxIcon' alt=''></img>
                     <p className='sevixeBoxP '>2D Plan</p>
+                    <p className='priceBox'><s className='fontFamily priceBox'>Rs.3000</s><span className='fontFamily priceBox'>Rs.999</span></p>
                 </a>
                 <a className="serviceBox" href="#Elevation">
-                    <img src={elevation} className='serviceBoxIcon' alt=''></img>
+                    <img src= {require("../assets/elevation2.jpeg")} className='serviceBoxIcon' alt=''></img>
                     <p className='sevixeBoxP ' >Elevation</p>
+                    <p className='priceBox'><s className='fontFamily priceBox'>Rs.5000</s><span className='fontFamily priceBox'>Rs.1999</span></p>
+
                 </a>
                 <a className="serviceBox" href="#Interior">
                     <img src={Interior} className='serviceBoxIcon' alt=''></img>
                     <p className='sevixeBoxP '>Interior</p>
+                    <p className='priceBox'><s className='fontFamily priceBox'>Rs.5000</s><span className='fontFamily priceBox'>Rs.1999</span></p>
+
                 </a>
                 <a className="serviceBox" href="#3Dplan">
                     <img src={plan} className='serviceBoxIcon' alt=''></img>
                     <p className='sevixeBoxP '>3D Plan</p>
+                    <p className='priceBox'><s className='fontFamily priceBox'>Rs.3000</s><span className='fontFamily priceBox'>Rs.1500</span></p>
+
                 </a>
                 <a className="serviceBox" href='#WalkTrough'>
                     <img src={WalkTrough} className='serviceBoxIcon' alt=''></img>
                     <p className='sevixeBoxP '>Walk Trough</p>
+                    <p className='priceBox'><s className='fontFamily priceBox'>Rs.4000</s><span className='fontFamily priceBox'>Rs.1999</span></p>
+
                 </a>
             </div>
         </section>
       </div>
     );
-  }
+}
   
-  export default Services;
+export default Services;
   
